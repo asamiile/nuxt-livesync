af8264aef7b2142d52ec0274457e7ad5d26c82d4
asamiile <asamin47@gmail.com>
2025-09-25 00:06:06 +0900

    fix error page layout.

--- error.vue
+++ error.vue
@@ -1,11 +1,17 @@
 <template>
   <NuxtLayout name="default">
-    <div class="text-center">
-      <h1 class="text-4xl font-bold mb-4">{{ error.statusCode }}</h1>
-      <p class="text-xl mb-8">{{ message }}</p>
-      <NuxtLink to="/" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Go back home
-      </NuxtLink>
+    <div class="flex items-center justify-center text-center">
+      <div class="w-full max-w-md space-y-8 rounded-lg bg-card text-card-foreground p-8 shadow-md">
+        <h1 class="mt-6 text-center text-3xl font-extrabold">{{ error.statusCode }}</h1>
+        <div>
+          <p class="text-xl mb-8">{{ message }}</p>
+          <NuxtLink to="/">
+            <Button>
+              Go back home
+            </Button>
+          </NuxtLink>
+        </div>
+      </div>
     </div>
   </NuxtLayout>
 </template>
@@ -13,6 +19,7 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import type { NuxtError } from '#app'
+import { Button } from '@/components/ui/button'
 
 const props = defineProps({
   error: Object as () => NuxtError
