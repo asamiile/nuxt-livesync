3acbe4ff98394a959a61f2b66d2100774f006f64
asamiile <asamin47@gmail.com>
2025-09-26 20:44:45 +0900

    revert.

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,5 @@ export default defineVitestConfig({
   test: {
     globals: true,
     environment: 'nuxt',
-    include: ['test/unit/**/*.spec.ts', 'test/nuxt/**/*.spec.ts'],
-    exclude: ['node_modules', 'test/e2e/**'],
   },
 })
